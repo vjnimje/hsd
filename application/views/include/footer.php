5a1994d95b8b427b805ef77ed5a8e4f1fe0d5318
Vijay Nimje <vjnimje@yahoo.com>
2021-11-29 12:50:50 +0530

    map api removed

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -152,7 +152,7 @@
   <script src="<?php echo base_url();?>assets/js/bootstrap-datepicker.js"></script>
   <script src="<?php echo base_url();?>assets/js/jquery.timepicker.min.js"></script>
   <script src="<?php echo base_url();?>assets/js/scrollax.min.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src=""></script>
   <script src="<?php echo base_url();?>assets/js/google-map.js"></script>
   <script src="<?php echo base_url();?>assets/js/main.js"></script>
   <script src="<?php echo base_url();?>assets/js/carousel.js"></script>
